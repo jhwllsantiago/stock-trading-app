a000743c563e9b387a76416de75303f6753bf350
Jhowell Santiago <jhwllsantiago@gmail.com>
2022-12-20 17:17:44 +0800

    added stock scaffold, added db seed for users and stocks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users = User.create([
+  {email: "user@user.com", password: "password", password_confirmation: "password", first_name: "John", last_name: "Doe", balance: 150.00, role: 0, active: true},
+  {email: "admin@admin.com", password: "password", password_confirmation: "password", first_name: "Admin John", last_name: "Doe", balance: 0.0, role: 1, active: true},
+  ])
+stocks = Stock.create([{ price: 15.12, company: "ABC Company" }, { price: 10.11, company: "DEF Company" }, { price: 1.01, company: "GHI Company" }])
