96a74f83cbea5a02bf682a7ff8c26e72ce512d63
shailaksy <shailaksy@gmail.com>
2023-01-06 21:05:02 +0800

    added user management for admin

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,15 @@ Rails.application.routes.draw do
   }
   root "pages#landing"
   get "error", to: "pages#error"
+  get "admin", to: "admin#dashboard"
+  get "admin/:id", to: "admin#show", as: "show_user"
+  # get "admin/:id/edit", to: "admin#edit", as: "edit_user"
+  devise_scope :user do
+    get "admin/:id/edit", to: "users/registrations#edit", as: "edit_user"
+  end
+  patch "admin/:id", to: "admin#update", as: "update_user"
+  delete "admin/:id", to: "admin#destroy", as: "destroy_user"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
