ca7a362ce133d1d2f0d5874fb06a48175955346d
Vlake <apulivic@gmail.com>
2023-01-07 09:12:15 +0800

    made user/sesssions#new as root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
   resources :stocks do
     resources :orders
+
+    
   end
   devise_for :users, controllers: {
     sessions: "users/sessions",
     registrations: "users/registrations"
+
   }
-  root "pages#landing"
+  
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+  
   get "error", to: "pages#error"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root 'devise/sessions#new'
 end
