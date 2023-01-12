f325b5bd73844143ec11db58140835b9bcda284d
shailaksy <shailaksy@gmail.com>
2023-01-12 19:27:17 +0800

    redirects admin sign in to admin dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def landing
     redirect_to stocks_path if current_user and current_user.trader?
+    redirect_to admin_path if current_user and current_user.admin?
   end
 
   def error
