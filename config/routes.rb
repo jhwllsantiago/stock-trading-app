a2b3970152af9b1b96c3fc2176d792aefb2d9ac5
Vlake <apulivic@gmail.com>
2023-01-12 19:34:57 +0800

    commit for development merge

--- config/routes.rb
+++ config/routes.rb
@@ -6,22 +6,12 @@ Rails.application.routes.draw do
     sessions: "users/sessions",
     registrations: "users/registrations"
   }
-
+  root "devise/sessions#new"
   get "error", to: "pages#error"
   get "admin", to: "admin#dashboard"
   get "admin/manage/:id", to: "admin#show", as: "show_user"
-  # get "admin/:id/edit", to: "admin#edit", as: "edit_user"
-  devise_scope :user do
-    get "admin/manage/:id/edit", to: "users/registrations#edit", as: "edit_user"
-        authenticated :user do
-        root :to => 'stocks#index', as: :authenticated_root
-      end
-      unauthenticated :user do
-        root :to => 'devise/sessions#new', as: :unauthenticated_root
-      end
-  end
+  get "admin/manage/:id/edit", to: "admin#edit", as: "edit_user"
   patch "admin/manage/:id", to: "admin#update", as: "update_user"
   delete "admin/manage/:id", to: "admin#destroy", as: "destroy_user"
   patch "admin/manage/:id/approve", to: "admin#approve", as: "approve_user"
-  get "admin/users/:id", to: "admin#user_row", as: "user_row"
 end
