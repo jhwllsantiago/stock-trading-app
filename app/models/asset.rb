ddee03ace2028b4c18b0a7198631a4bf74ad01d0
Jhowell Santiago <jhwllsantiago@gmail.com>
2023-01-17 19:49:03 +0800

    added asset model

---
+++ app/models/asset.rb
@@ -0,0 +1,5 @@
+class Asset < ApplicationRecord
+  belongs_to :user
+  belongs_to :stock
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_12_121608) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_17_113425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "assets", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "user_id", null: false
+    t.bigint "stock_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["stock_id"], name: "index_assets_on_stock_id"
+    t.index ["user_id"], name: "index_assets_on_user_id"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer "action"
@@ -54,6 +64,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_12_121608) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "assets", "stocks"
+  add_foreign_key "assets", "users"
   add_foreign_key "orders", "stocks"
   add_foreign_key "orders", "users"
 end
