16051c3ebb466cbbd1f2774bb488c88a98ece0e8
Vlake <apulivic@gmail.com>
2023-01-20 12:43:04 +0800

    adding design and layout to stock index

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,6 +4,7 @@ class StocksController < ApplicationController
   # GET /stocks or /stocks.json
   def index
     @stocks = Stock.all
+    @orders = Order.all
   end
 
   # GET /stocks/1 or /stocks/1.json
