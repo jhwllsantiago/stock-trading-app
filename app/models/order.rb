8038419dc56ead83b8bc25f98e6242b1784ff21b
Jhowell Santiago <jhwllsantiago@gmail.com>
2023-01-20 23:48:05 +0800

    fixed price_updater, set condition to query new price

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,8 @@
 class Order < ApplicationRecord
-  scope :buy, -> { where("action = 0") }
-  scope :sell, -> { where("action = 1") }
-  scope :pending, -> { where("status = 0") }
+  scope :buy, -> { where(action: 0) }
+  scope :sell, -> { where(action: 1) }
+  scope :pending, -> { where(status: 0) }
+  scope :by_admin, -> { where(user: User.find_by(role: 1), action: 1, status: 0) }
   enum action: [:buy, :sell]
   enum status: [:pending, :success, :expired]
   belongs_to :user
